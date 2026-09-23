f5e5c1f672d47dfbaecb4d46949b41e6eb575a8f
D.A.R.K. <davysambo@gmail.com>
2026-09-23 15:02:19 +0000

    Publish corrected QCM catalogue

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Stratégie Network-First : Priorité aux fichiers frais, secours hors-ligne immédiat
  */
 
-const CACHE_NAME = 'duc-concours-v20';
+const CACHE_NAME = 'duc-concours-v21';
 
 const FICHIERS_ESSENTIELS = [
   './',
